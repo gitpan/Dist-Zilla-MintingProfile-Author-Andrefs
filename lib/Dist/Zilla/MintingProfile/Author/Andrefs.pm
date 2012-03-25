ad6119da41fbf9f82407b295c5b1c4fd5d7ef374
André Fernandes dos Santos <andrefs@cpan.org>
2012-03-25 03:12:14 +0000

    Import of ANDREFS/Dist-Zilla-MintingProfile-Author-Andrefs-0.01_08 from CPAN.
    gitpan-cpan-distribution: Dist-Zilla-MintingProfile-Author-Andrefs
    gitpan-cpan-version:      0.01_08
    gitpan-cpan-path:         ANDREFS/Dist-Zilla-MintingProfile-Author-Andrefs-0.01_08.tar.gz
    gitpan-cpan-author:       ANDREFS
    gitpan-cpan-maturity:     developer

--- lib/Dist/Zilla/MintingProfile/Author/Andrefs.pm
+++ lib/Dist/Zilla/MintingProfile/Author/Andrefs.pm
@@ -2,7 +2,7 @@ use strict;
 use warnings;
 package Dist::Zilla::MintingProfile::Author::Andrefs;
 {
-  $Dist::Zilla::MintingProfile::Author::Andrefs::VERSION = '0.01_07';
+  $Dist::Zilla::MintingProfile::Author::Andrefs::VERSION = '0.01_08';
 }
 
 # ABSTRACT: Andrefs's Minting Profile
@@ -26,7 +26,7 @@ Dist::Zilla::MintingProfile::Author::Andrefs - Andrefs's Minting Profile
 
 =head1 VERSION
 
-version 0.01_07
+version 0.01_08
 
 =head1 AUTHOR
 
