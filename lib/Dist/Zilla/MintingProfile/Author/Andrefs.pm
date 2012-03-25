99ab3de3e49d055d83d04567dbb60c595e4e4d0c
André Fernandes dos Santos <andrefs@cpan.org>
2012-03-25 02:18:23 +0000

    Import of ANDREFS/Dist-Zilla-MintingProfile-Author-Andrefs-0.01_02 from CPAN.
    gitpan-cpan-distribution: Dist-Zilla-MintingProfile-Author-Andrefs
    gitpan-cpan-version:      0.01_02
    gitpan-cpan-path:         ANDREFS/Dist-Zilla-MintingProfile-Author-Andrefs-0.01_02.tar.gz
    gitpan-cpan-author:       ANDREFS
    gitpan-cpan-maturity:     developer

---
+++ lib/Dist/Zilla/MintingProfile/Author/Andrefs.pm
@@ -0,0 +1,43 @@
+use strict;
+use warnings;
+package Dist::Zilla::MintingProfile::Author::Andrefs;
+{
+  $Dist::Zilla::MintingProfile::Author::Andrefs::VERSION = '0.01_02';
+}
+
+# ABSTRACT: Andrefs's Minting Profile
+ 
+use Moose;
+use namespace::autoclean;
+with 'Dist::Zilla::Role::MintingProfile::ShareDir';
+ 
+ 
+__PACKAGE__->meta->make_immutable;
+no Moose;
+
+1;
+
+__END__
+=pod
+
+=head1 NAME
+
+Dist::Zilla::MintingProfile::Author::Andrefs - Andrefs's Minting Profile
+
+=head1 VERSION
+
+version 0.01_02
+
+=head1 AUTHOR
+
+Andre Santos <andrefs@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2012 by Andre Santos.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
